d89791986864aada1f0ac7e23c95ad5c266dc3d2
4-Abdullah <abdullahmaliksher@gmail.com>
2024-09-29 13:22:07 +0500

    added

--- config/database.js
+++ config/database.js
@@ -93,25 +93,52 @@
 
 const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       connectionString: env('DATABASE_URL'),
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi'),
+//       ssl: env.bool('DATABASE_SSL', false) && {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+//       },
+//       schema: env('DATABASE_SCHEMA', 'public'),
+//     },
+//     pool: {
+//       min: env.int('DATABASE_POOL_MIN', 2),
+//       max: env.int('DATABASE_POOL_MAX', 10),
+//     },
+//     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+//   },
+// });
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-      },
       schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env('DATABASE_SSL', false),
     },
     pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 2000,
     },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    acquireConnectionTimeout: 1000000,
+    debug: false,
   },
 });
+
